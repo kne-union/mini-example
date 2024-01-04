9955bfeaeb6d4545d0401b47f213758f66b75a5e
Lin <linzhipeng@fescoadecco.com>
2024-01-04 10:38:42 +0800

    格式化example显示

--- temp/src/app.js
+++ temp/src/app.js
@@ -1,6 +1,8 @@
 import preset from './preset';
 import {Global} from '@kne/mini-core';
 import './app.scss';
+import './html-content.scss';
+import './code.css';
 
 function App({children}) {
     return <Global preset={preset}>{children}</Global>
