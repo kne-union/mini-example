83dafdd270f0b46b290a5b3ed5e6ed3c8b5eeb21
zhipenglin <sunandmoon001@163.com>
2024-03-27 18:12:54 +0800

    默认隐藏源代码

--- temp/src/pages/detail/index.js
+++ temp/src/pages/detail/index.js
@@ -1,7 +1,8 @@
+import {useState} from 'react';
 import {Layout} from '@kne/mini-core';
 import componentsDoc from 'components-doc';
 import {useRouter} from '@tarojs/taro';
-import {Card} from '@kne/antd-taro';
+import {Button, Card} from '@kne/antd-taro';
 import {View} from '@tarojs/components';
 import Prism from 'prismjs';
 import escape2Html from '../../common/escape2Html';
@@ -12,6 +13,7 @@ const Components = () => {
     const detailInfo = componentsDoc.example.list[params.index];
     const scopeHtml = Prism.highlight(detailInfo.scope, Prism.languages.javascript, 'jsx');
     const codeHtml = Prism.highlight(detailInfo.code, Prism.languages.javascript, 'jsx');
+    const [open, setOpen] = useState(false);
 
     return <Layout header={{title: detailInfo.title}}>
         <Card title="说明">
@@ -20,10 +22,12 @@ const Components = () => {
         <Card title="示例">
             {detailInfo.component()}
         </Card>
-        <Card title="源代码">
-            <View dangerouslySetInnerHTML={{
+        <Card title="源代码" extra={<Button size="small" fill='none' onClick={() => {
+            setOpen((open) => !open);
+        }}>{open ? '</隐藏>' : '<显示>'}</Button>}>
+            {open && <View dangerouslySetInnerHTML={{
                 __html: `<div class="language-jsx">${scopeHtml}<br/>${escape2Html(codeHtml)}</div>`
-            }} className='html-content'/>
+            }} className='html-content'/>}
         </Card>
     </Layout>;
 };
